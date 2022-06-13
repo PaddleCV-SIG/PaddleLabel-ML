2b7040b6417510a1b0bcab567de8f776cd775c31
Lin Han <me@linhan.email>
2022-06-12 22:53:55 -0400

    fix(serve)

--- pplabel_ml/__main__.py
+++ pplabel_ml/__main__.py
@@ -17,24 +17,24 @@ connexion_app.add_api(
 
 CORS(connexion_app.app)
 
-# connexion_app.run(host="0.0.0.0", port=1234, debug=True)
+connexion_app.run(host="0.0.0.0", port=1234, debug=True)
 
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
+# from werkzeug.middleware.dispatcher import DispatcherMiddleware
+# from werkzeug.serving import run_simple
 
-from visualdl.server.args import ParseArgs
-import visualdl.server.app as vdlApp
+# from visualdl.server.args import ParseArgs
+# import visualdl.server.app as vdlApp
 
-args = {"logdir": ".", "public_path": "/visualdl"}
-vdlApp = vdlApp.create_app(ParseArgs(**args))
+# args = {"logdir": ".", "public_path": "/visualdl"}
+# vdlApp = vdlApp.create_app(ParseArgs(**args))
 
+# # application = DispatcherMiddleware(vdlApp, {"/model": connexion_app})
 # application = DispatcherMiddleware(vdlApp, {"/model": connexion_app})
-application = DispatcherMiddleware(vdlApp, {"/model": connexion_app})
 
 
-def run():
-    run_simple("0.0.0.0", 1234, application, use_reloader=True, threaded=True)
+# def run():
+#     run_simple("0.0.0.0", 1234, application, use_reloader=True, threaded=True)
 
 
-if __name__ == "__main__":
-    run()
+# if __name__ == "__main__":
+#     run()
