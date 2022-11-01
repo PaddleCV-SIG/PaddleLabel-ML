91c22a41b6beb4e7c1d32e10e39ce80531996943
Lin Han <me@linhan.email>
2022-11-01 02:44:53 -0400

    fix(ci)

--- paddlelabel_ml/util.py
+++ paddlelabel_ml/util.py
@@ -9,7 +9,7 @@ HERE = Path(__file__).parent.absolute()
 
 def get_models(model_fdr="model"):
     model_idxs = importlib.resources.contents(f"paddlelabel_ml.{model_fdr}")
-    model_idxs = [idx for idx in model_idxs if idx[0] != "_" and idx[0] != "."  ]
+    model_idxs = [idx for idx in model_idxs if idx[0] != "_" and idx[0] != "."]
     model_idxs.remove("base")
 
     models = {}
